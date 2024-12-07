7c4bc70e5f9a3304c9855921791d14a4aa2cde30
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-12-07 10:54:06 +0500

    Initial Commit

---
+++ MDSwiftUI/MDSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDSwiftUIApp.swift
+//  MDSwiftUI
+//
+//  Created by Adkhamjon Rakhimov on 07/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MDSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
