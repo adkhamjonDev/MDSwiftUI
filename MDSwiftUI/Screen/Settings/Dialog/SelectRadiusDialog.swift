edf8b729563d16c9a7ca24c416cdac60cfa26f80
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-12-13 16:56:29 +0500

    settings screen

---
+++ MDSwiftUI/Screen/Settings/Dialog/SelectRadiusDialog.swift
@@ -0,0 +1,18 @@
+//
+//  SelectRadiusDialog.swift
+//  MDSwiftUI
+//
+//  Created by Adkhamjon Rakhimov on 13/12/24.
+//
+
+import SwiftUI
+
+struct SelectRadiusDialog: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectRadiusDialog()
+}
